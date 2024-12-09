a80bf2d0e2b045538d026fc207ab6ca5df2e6f0d
Fred78290 <frederic.boltz@gmail.com>
2024-12-09 16:11:58 +0100

    WIP

--- Sources/caked/CloudImage/Certificate.swift
+++ Sources/caked/CloudImage/Certificate.swift
@@ -4,6 +4,7 @@ import Crypto
 import SwiftASN1
 import X509
 import _CryptoExtras
+import GRPCLib
 
 struct CypherKeyGeneratorError: Error {
 	let description: String
@@ -13,25 +14,124 @@ struct CypherKeyGeneratorError: Error {
 	}
 }
 
+public struct PrivateKeyModel {
+	var publicKey: SecKey
+	var privateKey: SecKey
+
+	init(publicKey: SecKey, privateKey: SecKey) {
+		self.publicKey = publicKey
+		self.privateKey = privateKey
+	}
+
+	public func save(privateURL: URL , publicURL: URL) throws {
+		let privateKey = try self.privateKeyString()
+		let publicKey = try self.publicKeyString()
+
+		FileManager.default.createFile(atPath: privateURL.path, contents: privateKey.data(using: .ascii), attributes: [.posixPermissions : 0o600])
+		FileManager.default.createFile(atPath: publicURL.path, contents: publicKey.data(using: .ascii), attributes: [.posixPermissions : 0o644])
+	}
+
+	public func publicKeyString() throws -> String {
+		var error: Unmanaged<CFError>?
+
+		guard let externalRepresentation = SecKeyCopyExternalRepresentation(self.publicKey, &error) else {
+			throw error!.takeRetainedValue() as Error
+		}
+
+		let data = [UInt8](externalRepresentation as Data)
+
+		var sshRsa: [UInt8] = [
+			0x00, 0x00, 0x00, 0x07,
+			0x73, 0x73, 0x68, 0x2d, 0x72, 0x73, 0x61,
+			0x00, 0x00, 0x00, 0x03,
+			0x01, 0x00, 0x01,
+			0x00, 0x00
+		]
+
+		let pubKey = [UInt8](data[6...data.count-6])
+
+		sshRsa.append(contentsOf: pubKey)
+
+		return "ssh-rsa " + Data(sshRsa).base64EncodedString()
+	}
+
+	public func privateKeyString() throws -> String {
+		var error: Unmanaged<CFError>?
+
+		guard let externalRepresentation = SecKeyCopyExternalRepresentation(self.privateKey, &error) else {
+			throw error!.takeRetainedValue() as Error
+		}
+
+		let data = externalRepresentation as Data
+
+		return "-----BEGIN RSA PRIVATE KEY-----\n" + data.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters) + "\n-----END RSA PRIVATE KEY-----"
+	}
+}
+
+
 public struct RSAKeyGenerator {
-	private var privateKey: _RSA.Signing.PrivateKey
+	private var privateKey: PrivateKeyModel
 
 	var publicKeyString : String {
 		get {
-			return self.privateKey.publicKey.pemRepresentation
+			return try! self.privateKey.publicKeyString()
 		}
 	}
 
 	init() throws {
-		self.privateKey = try _RSA.Signing.PrivateKey(keySize: .bits2048)
+		self.privateKey = try Self.generateKey()
 	}
 
 	public func save(privateURL: URL , publicURL: URL) throws {
-		let privateKey = self.privateKey.pemRepresentation
-		let publicKey = self.privateKey.publicKey.pemRepresentation
+		try privateKey.save(privateURL: privateURL, publicURL: publicURL)
+	}
 
-		FileManager.default.createFile(atPath: privateURL.path, contents: privateKey.data(using: .ascii), attributes: [.posixPermissions : 0o600])
-		FileManager.default.createFile(atPath: publicURL.path, contents: publicKey.data(using: .ascii), attributes: [.posixPermissions : 0o644])
+	static func generateKey() throws -> PrivateKeyModel {
+
+		var error: Unmanaged<CFError>?
+
+		let publicKeyAttr: CFDictionary =
+			[
+				kSecAttrIsPermanent: false,
+				kSecAttrLabel: Utils.cakerSignature,
+				kSecAttrApplicationLabel: Utils.cakerSignature,
+				kSecClass: kSecClassKey,
+				kSecReturnData: kCFBooleanTrue ?? true,
+				kSecAttrIsExtractable: kCFBooleanTrue ?? true,
+				kSecAttrProtocol: kSecAttrProtocolSSH,
+			] as CFDictionary  // added this value
+
+		let privateKeyAttr: CFDictionary =
+			[
+				kSecAttrIsPermanent: false,
+				kSecAttrLabel: Utils.cakerSignature,
+				kSecAttrApplicationLabel: Utils.cakerSignature,
+				kSecClass: kSecClassKey,  // added this value
+				kSecReturnData: kCFBooleanTrue ?? true,
+				kSecAttrIsExtractable: kCFBooleanTrue ?? true,
+				kSecAttrProtocol: kSecAttrProtocolSSH,
+			] as CFDictionary  // added this value
+
+		let parameters: CFDictionary =
+			[
+				kSecAttrKeyType: kSecAttrKeyTypeRSA,
+				kSecAttrApplicationLabel: Utils.cakerSignature,
+				kSecAttrKeySizeInBits: 4096,
+				kSecPublicKeyAttrs: publicKeyAttr,
+				kSecPrivateKeyAttrs: privateKeyAttr,
+				kSecAttrProtocol: kSecAttrProtocolSSH,
+			] as CFDictionary
+
+
+		guard let privateKey = SecKeyCreateRandomKey(parameters, &error) else {
+			throw error!.takeRetainedValue() as Error
+		}
+
+		guard let publicKey = SecKeyCopyPublicKey(privateKey) else {
+			throw CypherKeyGeneratorError("Unable to get public key")
+		}
+
+		return PrivateKeyModel(publicKey: publicKey, privateKey: privateKey)
 	}
 
 	static func generateClientServerCertificate(subject: String, numberOfYears: Int,
