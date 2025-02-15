eb7c8ddf3ca592ac1671f98208a666cda759887b
Fred78290 <frederic.boltz@gmail.com>
2025-02-15 10:16:19 +0100

    WIP

--- Sources/caked/Handlers/TemplateHandler.swift
+++ Sources/caked/Handlers/TemplateHandler.swift
@@ -24,6 +24,46 @@ struct TemplateHandler: CakedCommand {
 		let deleted: Bool
 	}
 
+	static func cleanCloudInit(location: VMLocation, config: CakeConfig) throws {
+		let semaphore = DispatchSemaphore(value: 0)
+
+		Task {
+			defer {
+				semaphore.signal()
+			}
+
+			let runningIP = try await StartHandler.startVM(vmLocation: location, waitIPTimeout: 60, foreground: false)
+
+			if runningIP.count > 0 {
+				let home = try Home(asSystem: asSystem)
+				let cloudInitCleanup = [
+					"rm -f /etc/cloud/cloud.cfg.d/50-curtin-networking.cfg",
+					"rm /etc/netplan/*",
+					"cloud-init clean",
+					"cloud-init clean -l",
+					"rm -rf /etc/apparmor.d/cache/* /etc/apparmor.d/cache/.features",
+					"/usr/bin/truncate --size 0 /etc/machine-id",
+					"rm -f /snap/README",
+					"find /usr/share/netplan -name __pycache__ -exec rm -r {} +",
+					"rm -rf /var/cache/pollinate/seeded /var/cache/snapd/* /var/cache/motd-news",
+					"rm -rf /var/lib/cloud /var/lib/dbus/machine-id /var/lib/private /var/lib/systemd/timers /var/lib/systemd/timesync /var/lib/systemd/random-seed",
+					"rm -f /var/lib/ubuntu-release-upgrader/release-upgrade-available",
+					"rm -f /var/lib/update-notifier/fsck-at-reboot /var/lib/update-notifier/hwe-eol",
+					"find /var/log -type f -exec rm -f {} +",
+					"rm -r /tmp/* /tmp/.*-unix /var/tmp/* /var/lib/apt/*",
+					"/bin/sync",
+					"shutdown now"
+				]
+
+				let ssh = try SSH(host: runningIP)
+				try ssh.authenticate(username: config.configuredUser, privateKey: home.sshPrivateKey.path(), publicKey: home.sshPublicKey.path(), passphrase: "")
+				try ssh.execute("sudo sh -c '\(cloudInitCleanup.joined(separator: ";"))'")
+			}
+		}
+
+		semaphore.wait()
+	}
+
 	static func createTemplate(sourceName: String, templateName: String, asSystem: Bool) throws -> CreateTemplateReply {
 		let storage = StorageLocation(asSystem: asSystem, template: true)
 		let location: VMLocation = try StorageLocation(asSystem: asSystem).find(sourceName)
@@ -40,6 +80,12 @@ struct TemplateHandler: CakedCommand {
 		}
 
 		if location.status != .running {
+			let config = try location.config()
+
+			if config.os == .linux && config.cloudInit {
+				throw cleanCloudInit(location: location, config: config)
+			}
+
 			let templateLocation = storage.location(templateName)
 
 			try FileManager.default.createDirectory(at: templateLocation.rootURL, withIntermediateDirectories: true)
