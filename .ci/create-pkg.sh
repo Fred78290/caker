5cc062f4c5da5b3d422cc6a430d8a988721fc279
Fred78290 <frederic.boltz@gmail.com>
2025-04-20 15:54:33 +0200

    WIP

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -7,7 +7,15 @@ if [ -f .env ]; then
 	source .env
 fi
 
+rm -rf ${PKGDIR} ./.ci/Caker-$VERSION.pkg
+
 mkdir -p ${PKGDIR}/bin ${PKGDIR}/Contents/MacOS ${PKGDIR}/Contents/Resources
+
+codesign --sign "Developer ID Application: Frederic BOLTZ (${TEAM_ID})" --options runtime --entitlements Resources/release.entitlements --force .build/x86_64-apple-macosx/release/caked
+codesign --sign "Developer ID Application: Frederic BOLTZ (${TEAM_ID})" --options runtime --entitlements Resources/release.entitlements --force .build/arm64-apple-macosx/release/caked
+
+codesign --sign "Developer ID Application: Frederic BOLTZ (${TEAM_ID})" --options runtime --entitlements Resources/release.entitlements --force .build/x86_64-apple-macosx/release/cakectl
+codesign --sign "Developer ID Application: Frederic BOLTZ (${TEAM_ID})" --options runtime --entitlements Resources/release.entitlements --force .build/arm64-apple-macosx/release/cakectl
 
 lipo -create .build/x86_64-apple-macosx/release/caked .build/arm64-apple-macosx/release/caked -output ${PKGDIR}/bin/caked
 lipo -create .build/x86_64-apple-macosx/release/cakectl .build/arm64-apple-macosx/release/cakectl -output ${PKGDIR}/bin/cakectl
@@ -18,14 +26,18 @@ ln -s ../../bin/cakectl .
 popd >/dev/null
 
 cp -c ${CURDIR}/Resources/caker.provisionprofile ${PKGDIR}/Contents/embedded.provisionprofile
-cp -c ${CURDIR}/Resources/cakectl.plist ${PKGDIR}/Contents/Info.plist
+cp -c ${CURDIR}/Resources/caked.plist ${PKGDIR}/Contents/Info.plist
 cp -c ${CURDIR}/Resources/CakedAppIcon.png ${PKGDIR}/Contents/Resources/AppIcon.png
+
+#codesign --sign "Developer ID Application: Frederic BOLTZ (${TEAM_ID})" --options runtime --entitlements Resources/release.entitlements --force .ci/pkg/Caker.app
 
 if [ -n "$1" ]; then
 	KEYCHAIN_OPTIONS="--keychain $1"
 else
 	KEYCHAIN_OPTIONS=
 fi
+
+echo "Creating package for version $VERSION, team ID ${TEAM_ID}"
 
 pkgbuild --root .ci/pkg/ \
 		--identifier com.aldunelabs.caker \
@@ -36,10 +48,14 @@ pkgbuild --root .ci/pkg/ \
 		${KEYCHAIN_OPTIONS} \
 		"./.ci/Caker-$VERSION.pkg"
 
+echo "Submitting package for notarization"
+
 xcrun notarytool submit "./.ci/Caker-$VERSION.pkg" ${KEYCHAIN_OPTIONS} \
 		--apple-id ${APPLE_ID} \
 		--team-id ${TEAM_ID} \
 		--password "${APP_PASSWORD}" \
 		--wait
+
+echo "Stapling package"
 
 xcrun stapler staple "./.ci/Caker-$VERSION.pkg"
