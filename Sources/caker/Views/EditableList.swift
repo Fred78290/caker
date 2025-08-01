635ff9aa9df5240fa5b9e09f454d8db6fbcb1504
Fred78290 <frederic.boltz@gmail.com>
2025-08-01 15:57:59 +0200

    Enables disabling edit actions and improves UI
    Allows disabling the edit, add and delete actions in the editable list.
    Improves the user interface of the empty list state using GeometryReader.

--- Sources/caker/Views/EditableList.swift
+++ Sources/caker/Views/EditableList.swift
@@ -17,11 +17,13 @@ struct OnEditItemListViewModifier<Element: Hashable, SomeView: View>: ViewModifi
 	@State private var displayAddItemView: Bool = false
 	@State private var displayEditItemView: Bool = false
 	@Binding private var selection: Element?
+	@Binding private var disabled: Bool
 
-	init(selection: Binding<Element?>, onEditItem: @escaping (Element?) -> SomeView, onDeleteItem: DeleteItemAction?) {
+	init(selection: Binding<Element?>, disabled: Binding<Bool>, onEditItem: @escaping (Element?) -> SomeView, onDeleteItem: DeleteItemAction?) {
 		self.editItemClosure = onEditItem
 		self.deleteItem = onDeleteItem
 		self._selection = selection
+		self._disabled = disabled
 	}
 
 	func body(content: Content) -> some View {
@@ -33,21 +35,30 @@ struct OnEditItemListViewModifier<Element: Hashable, SomeView: View>: ViewModifi
 					displayAddItemView = true
 				}) {
 					Image(systemName: "plus")
-				}.buttonStyle(.borderless).font(.headline)
+				}
+				.buttonStyle(.borderless)
+				.font(.headline)
+				.disabled(self.disabled)
 				
 				if let deleteItem = deleteItem {
 					Button(action: {
 						deleteItem()
 					}) {
 						Image(systemName: "minus")
-					}.buttonStyle(.borderless).font(.headline).disabled(self.selection == nil)
+					}
+					.buttonStyle(.borderless)
+					.font(.headline)
+					.disabled(self.selection == nil || self.disabled)
 				}
 
 				Button(action: {
 					displayEditItemView = true
 				}) {
 					Image(systemName: "pencil")
-				}.buttonStyle(.borderless).font(.headline).disabled(self.selection == nil)
+				}
+				.buttonStyle(.borderless)
+				.font(.headline)
+				.disabled(self.selection == nil || self.disabled)
 
 				Spacer()
 			}
@@ -109,34 +120,37 @@ struct EditableList<Data: TotalCollection, Content: View>: View where Data.Eleme
 	}
 
 	var body: some View {
-		VStack {
-			if data.isEmpty {
-				if #available(macOS 14, *) {
-					ContentUnavailableView(
-						"List empty",
-						systemImage: "tray"
-					)
-				} else {
-					VStack(alignment: .center) {
-						Image(systemName: "tray").resizable().scaledToFit().frame(width: 48, height: 48).foregroundStyle(.gray)
-						Text("List empty").font(.largeTitle).foregroundStyle(.gray).multilineTextAlignment(.center)
+		GeometryReader { geom in
+			ScrollView {
+				VStack(alignment: .center) {
+					if data.isEmpty {
+						if #available(macOS 14, *) {
+							VStack(alignment: .center) {
+								ContentUnavailableView("List empty", systemImage: "tray")
+							}.frame(width: geom.size.width)
+						} else {
+							VStack(alignment: .center) {
+								Image(systemName: "tray").resizable().scaledToFit().frame(width: 48, height: 48).foregroundStyle(.gray)
+								Text("List empty").font(.largeTitle).fontWeight(.bold).foregroundStyle(.gray).multilineTextAlignment(.center)
+							}.frame(width: geom.size.width)
+						}
+					} else {
+						List(selection: $selection) {
+							ForEach($data, content: listItem)
+								.onMove { indexSet, offset in
+									data.move(fromOffsets: indexSet, toOffset: offset)
+								}
+								.onDelete { indexSet in
+									data.remove(atOffsets: indexSet)
+								}
+						}
+						.frame(height: geom.size.height)
+						.listStyle(.bordered(alternatesRowBackgrounds: true))
+						.clipShape(RoundedRectangle(cornerRadius: 6))
+						.overlay(RoundedRectangle(cornerRadius: 6).strokeBorder(Color(CGColor.init(gray: 0.8, alpha: 0.4)), lineWidth: 1))
 					}
 				}
-			} else {
-				List(selection: $selection) {
-					ForEach($data, content: listItem)
-						.onMove { indexSet, offset in
-							data.move(fromOffsets: indexSet, toOffset: offset)
-						}
-						.onDelete { indexSet in
-							data.remove(atOffsets: indexSet)
-						}
-				}
-				.frame(maxWidth: .infinity, maxHeight: .infinity)
-				.listStyle(.bordered(alternatesRowBackgrounds: true))
-				.clipShape(RoundedRectangle(cornerRadius: 6))
-				.overlay(RoundedRectangle(cornerRadius: 6).strokeBorder(Color(CGColor.init(gray: 0.8, alpha: 0.4)), lineWidth: 1))
-			}
+			}.frame(height: geom.size.height)
 		}
 	}
 
@@ -153,7 +167,7 @@ struct EditableList<Data: TotalCollection, Content: View>: View where Data.Eleme
 }
 
 extension View {
-	func onEditItem<Element: Hashable>(selection: Binding<Element?>, _ action: @escaping (Element?) -> some View, deleteItem: DeleteItemAction? = nil) -> some View {
-		modifier(OnEditItemListViewModifier(selection: selection, onEditItem: action, onDeleteItem: deleteItem))
+	func onEditItem<Element: Hashable>(selection: Binding<Element?>, disabled: Binding<Bool>, @ViewBuilder _ action: @escaping (Element?) -> some View, deleteItem: DeleteItemAction? = nil) -> some View {
+		modifier(OnEditItemListViewModifier(selection: selection, disabled: disabled, onEditItem: action, onDeleteItem: deleteItem))
 	}
 }
