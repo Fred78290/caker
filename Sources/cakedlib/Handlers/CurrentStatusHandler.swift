73569648ab58f5b641ff03ebe9b8bf15a94310b4
Fred78290 <frederic.boltz@gmail.com>
2026-02-12 09:48:45 +0100

    Refactors status updates with file monitoring
    Replaces polling with file monitoring for VM status and screenshot updates, improving responsiveness and reducing resource usage.
    
    This change introduces FileMonitor to react to changes in the VM's PID file and screenshot file, triggering status updates and screenshot retrievals only when necessary.
    Also, introduces a watcher class that implements FileDidChangeDelegate.

--- Sources/cakedlib/Handlers/CurrentStatusHandler.swift
+++ Sources/cakedlib/Handlers/CurrentStatusHandler.swift
@@ -9,10 +9,26 @@ import GRPCLib
 import GRPC
 import CakeAgentLib
 import NIO
+import FileMonitor
+import FileMonitorShared
 
 typealias AsyncThrowingStreamCakeAgentCurrentUsageReply = (stream: AsyncThrowingStream<CakeAgent.CurrentUsageReply, Error>, continuation: AsyncThrowingStream<CakeAgent.CurrentUsageReply, Error>.Continuation)
 
 public struct CurrentStatusHandler {
+	private class Watcher: FileDidChangeDelegate {
+		let location: VMLocation
+		let handler: (_ event: FileChangeEvent) -> Void
+
+		init(location: VMLocation, handler: @escaping (_ event: FileChangeEvent) -> Void) {
+			self.location = location
+			self.handler = handler
+		}
+
+		func fileDidChanged(event: FileMonitorShared.FileChangeEvent) {
+			handler(event)
+		}
+	}
+
 	static func currentUsage(helper: CakeAgentHelper, _ continuation: AsyncThrowingStream<Caked_Reply, Error>.Continuation) async throws {
 		let (stream, usage) = AsyncThrowingStream<CakeAgent.CurrentUsageReply, Error>.makeStream()
 		
@@ -50,18 +66,18 @@ public struct CurrentStatusHandler {
 	public static func currentStatus(on: EventLoop, vmname: String, frequency: Int32, responseStream: GRPCAsyncResponseStreamWriter<Caked_Reply>, client: CakeAgentConnection, runMode: Utils.RunMode) async throws {
 		let location: VMLocation = try StorageLocation(runMode: runMode).find(vmname)
 		var lastStatusSeen = location.status
-		
+
 		try await withThrowingTaskGroup(of: Void.self) { group in
 			var group = group
 			let (stream, continuation) = AsyncThrowingStream<Caked_Reply, Error>.makeStream()
-			
-			group.addTask { () -> Void in
-				let sleep = 1000_000_000 / UInt64(frequency)
-				
-				while Task.isCancelled == false {
+
+			func check(_ file: URL) -> Void {
+				if file.lastPathComponent == location.pidFile.lastPathComponent {
+					let status = location.status
+
 					continuation.yield(.with {
 						$0.status = .with {
-							switch location.status {
+							switch status {
 							case .running:
 								$0.currentStatus = .running
 							case .stopped:
@@ -71,22 +87,54 @@ public struct CurrentStatusHandler {
 							}
 						}
 					})
-					
-					if location.status == .running && lastStatusSeen == .stopped {
+
+					if status == .running && lastStatusSeen == .stopped {
 						group.addTask {
 							try await currentUsage(helper: try CakeAgentHelper(on: on, client: client.createClient()), continuation)
 						}
 					}
-					
-					try? await Task.sleep(nanoseconds: sleep)
+
+					lastStatusSeen = status
+				} else if file.lastPathComponent == location.screenshotURL.lastPathComponent {
+					if let png = try? Data(contentsOf: location.screenshotURL) {
+						continuation.yield(.with {
+							$0.status.screenshot = png
+						})
+					}
 				}
 			}
 
-			for try await reply in stream {
-				try await responseStream.send(reply)
+			let monitor = try FileMonitor(directory: location.rootURL, delegate: Watcher(location: location) { event in
+				switch event {
+				case .added(let file):
+					check(file)
+				case .deleted(let file):
+					check(file)
+				case .changed(let file):
+					check(file)
+				}
+			})
+
+			defer {
+				monitor.stop()
+				group.cancelAll()
 			}
-			
-			group.cancelAll()
+
+			try monitor.start()
+
+			if location.status == .running {
+				group.addTask {
+					try await currentUsage(helper: try CakeAgentHelper(on: on, client: client.createClient()), continuation)
+				}
+			}
+
+			try await withTaskCancellationHandler(operation: {
+				for try await reply in stream {
+					try await responseStream.send(reply)
+				}
+			}, onCancel: {
+				continuation.finish(throwing: CancellationError())
+			})
 		}
 	}
 	
