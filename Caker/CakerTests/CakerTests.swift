c68e11bfec0ee624a2e5d9d174533b645a3e96fc
Fred78290 <frederic.boltz@gmail.com>
2026-02-23 09:25:48 +0100

    Adds unit testing target to the project
    Configures a new testing target for the project, including necessary files, build phases, and dependencies.
    This allows for comprehensive unit testing of the application's functionality.

---
+++ Caker/CakerTests/CakerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CakerTests.swift
+//  CakerTests
+//
+//  Created by Frederic BOLTZ on 22/02/2026.
+//
+
+import Testing
+
+struct CakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
