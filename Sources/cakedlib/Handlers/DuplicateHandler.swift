d9bc5f1e2835b07a9f2395ded937ee846d21bb17
Fred78290 <frederic.boltz@gmail.com>
2026-02-28 15:29:06 +0100

    Enhances VM duplication with intelligent Cloud-Init handling
    Refactors VM duplication for improved robustness and clarity.
    Adds specific logic to clean and regenerate Cloud-Init ISOs for Linux VMs, ensuring unique network identities and instance IDs upon duplication.
    Improves error reporting by returning detailed reasons for duplication failures instead of throwing.
    Generalizes MAC address resetting and instance ID generation for all duplicated virtual machines.
    Introduces a `startMode` parameter for greater control over the duplication process.

--- Sources/cakedlib/Handlers/DuplicateHandler.swift
+++ Sources/cakedlib/Handlers/DuplicateHandler.swift
@@ -4,34 +4,71 @@ import NIOCore
 import Virtualization
 
 public struct DuplicateHandler {
-	public static func duplicate(from: String, to: String, resetMacAddress: Bool, runMode: Utils.RunMode) -> DuplicatedReply {
+	public static func duplicate(from: String, to: String, resetMacAddress: Bool, startMode: StartHandler.StartMode, runMode: Utils.RunMode) -> DuplicatedReply {
 		do {
 			let storageLocation = StorageLocation(runMode: runMode)
-			let fromLocation = try storageLocation.find(from)
+
+			guard var fromLocation = try? storageLocation.find(from) else {
+				return DuplicatedReply(from: from, to: to, duplicated: false, reason: "Source vm not found")
+			}
 
 			// Check if the VM exists
-			if fromLocation.status == .running {
-				throw ServiceError("VM \(from) is running")
+			guard fromLocation.status == .stopped else {
+				return DuplicatedReply(from: from, to: to, duplicated: false, reason: "Source vm is running or paused")
 			}
 
-			if storageLocation.exists(to) {
-				throw ServiceError("VM \(to) already exists")
+			guard storageLocation.exists(to) == false else {
+				return DuplicatedReply(from: from, to: to, duplicated: false, reason: "Target vm already exists")
 			}
 
-			let tmpLocation = try fromLocation.duplicateTemporary(runMode: runMode)
-			let config = try tmpLocation.config()
+			var config = try fromLocation.config()
 
-			// Change mac address and network mode
-			if resetMacAddress {
-				config.macAddress = VZMACAddress.randomLocallyAdministered()
-				config.networks = config.networks.map {
-					BridgeAttachement(network: $0.network, mode: $0.mode, macAddress: VZMACAddress.randomLocallyAdministered().string)
-				}
+			defer {
+				try? fromLocation.delete()
+			}
+
+			func resetMacAddress(_ location: VMLocation) throws -> CakeConfig {
+				let config = try fromLocation.config()
+				config.resetMacAddress()
+				config.instanceID = "i-\(String(format: "%x", Int(Date().timeIntervalSince1970)))"
 
 				try config.save()
+
+				return config
 			}
 
-			try storageLocation.relocate(to, from: tmpLocation)
+			if config.os == .linux && config.useCloudInit {
+				fromLocation = try TemplateHandler.cleanCloudInit(source: fromLocation, config: config, startMode: startMode, runMode: runMode)
+
+				// Change mac address and network mode
+				config = try resetMacAddress(fromLocation)
+
+				// Rebuild a new cloud-init iso with new mac address and network mode
+				let cdrom = URL(fileURLWithPath: cloudInitIso, relativeTo: fromLocation.diskURL)
+				let cloudInit = try CloudInit(
+					plateform: config.configuredPlatform,
+					userName: config.configuredUser,
+					password: config.configuredPassword,
+					mainGroup: config.configuredGroup,
+					otherGroups: config.configuredGroups,
+					clearPassword: config.clearPassword,
+					sshAuthorizedKeyPath: config.sshPrivateKeyPath,
+					vendorDataPath: nil,
+					userDataPath: nil,
+					networkConfigPath: nil,
+					netIfnames: config.ifname,
+					runMode: runMode)
+
+				try? cdrom.delete()
+				try cloudInit.createDefaultCloudInit(config: config, name: to, cdromURL: cdrom)
+			} else {
+				fromLocation = try fromLocation.duplicateTemporary(runMode: runMode)
+				
+				// Change mac address and network mode
+				config = try resetMacAddress(fromLocation)
+			}
+
+			try storageLocation.relocate(to, from: fromLocation)
 
 			return DuplicatedReply(from: from, to: to, duplicated: true, reason: "VM duplicated")
 		} catch {
