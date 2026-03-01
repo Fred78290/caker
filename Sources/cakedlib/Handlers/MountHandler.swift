5341fb51990720dcba24fcb7ec1e55703e5163ea
Fred78290 <frederic.boltz@gmail.com>
2026-03-01 12:20:04 +0100

    Removes VNC URL retrieval from MountHandler
    The static `VncURL` function is no longer part of `MountHandler`. This change aligns the handler's responsibilities more closely with mount operations.

--- Sources/cakedlib/Handlers/MountHandler.swift
+++ Sources/cakedlib/Handlers/MountHandler.swift
@@ -7,10 +7,6 @@ import NIO
 import Semaphore
 
 public struct MountHandler {
-	public static func VncURL(_ mode: VMRunServiceMode, location: VMLocation, runMode: Utils.RunMode) throws -> URL? {
-		return try createVMRunServiceClient(mode, location: location, runMode: runMode).vncURL()
-	}
-
 	public static func Mount(_ mode: VMRunServiceMode, location: VMLocation, mounts: DirectorySharingAttachments, runMode: Utils.RunMode) -> MountInfos {
 		do {
 			return try createVMRunServiceClient(mode, location: location, runMode: runMode).mount(mounts: mounts).withDirectorySharingAttachment(directorySharingAttachment: mounts)
