b782ff2fdbccd5a08a48c43fc7a672e46298c29b
Fred78290 <frederic.boltz@gmail.com>
2026-03-03 11:12:28 +0100

    Refactor config mapping and introduce public model
    Moves configuration type conversion logic from the `InfosHandler` to `CakedAliases`.
    Introduces `CakedConfiguration` as a public, codable virtual machine configuration model.
    Adds extensions to convert gRPC `Caked.Configuration` messages to internal types and the new public model.
    Simplifies the `InfosHandler` by centralizing type conversion responsibilities.

--- Sources/Grpc/CakedAliases.swift
+++ Sources/Grpc/CakedAliases.swift
@@ -1,6 +1,7 @@
 import Foundation
 import GRPC
 import CakeAgentLib
+import NIOPortForwarding
 
 public typealias CakedServiceClient = Caked_ServiceNIOClient
 
@@ -127,6 +128,278 @@ public typealias Caked_CurrentStatusRequest = Caked.CurrentStatusRequest
 public typealias Caked_GetScreenSizeRequest = Caked.GetScreenSizeRequest
 public typealias Caked_SetScreenSizeRequest = Caked.SetScreenSizeRequest
 public typealias Caked_InstallAgentRequest = Caked.InstallAgentRequest
+
+extension VirtualizedOS {
+	public init?(_ from: Caked.Configuration.VirtualizedOS) {
+		switch from {
+		case .darwin:
+			self = .darwin
+		case .linux:
+			self = .linux
+		default:
+			return nil
+		}
+	}
+}
+
+extension Architecture {
+	public init?(_ from: Caked.Configuration.Architecture) {
+		switch from {
+		case .amd64:
+			self = .amd64
+		case .arm64:
+			self = .arm64
+		default:
+			return nil
+		}
+	}
+}
+
+extension ImageSource {
+	public init?(_ from: Caked.Configuration.ImageSource) {
+		switch from {
+		case .raw:
+			self = .raw
+		case .cloud:
+			self = .cloud
+		case .oci:
+			self = .oci
+		case .template:
+			self = .template
+		case .stream:
+			self = .stream
+		case .iso:
+			self = .iso
+		case .ipsw:
+			self = .ipsw
+		default:
+			return nil
+		}
+	}
+}
+
+extension SupportedPlatform {
+	public init(_ from: Caked.Configuration.SupportedPlatform) {
+		switch from {
+		case .ubuntu:
+			self = .ubuntu
+		case .centos:
+			self = .centos
+		case .macos:
+			self = .macos
+		case .windows:
+			self = .windows
+		case .debian:
+			self = .debian
+		case .fedora:
+			self = .fedora
+		case .redhat:
+			self = .redhat
+		case .openSuse:
+			self = .openSUSE
+		case .alpine:
+			self = .alpine
+		default:
+			self = .unknown
+		}
+	}
+}
+
+extension DiskAttachement {
+	public init(_ from: Caked.Configuration.DiskAttachment) {
+		self.init()
+
+		self.diskPath = from.diskPath
+		self.diskOptions = DiskOptions(readOnly: from.diskOptions.readOnly, syncMode: from.diskOptions.syncMode, cachingMode: from.diskOptions.cachingMode)
+	}
+}
+
+extension DirectorySharingAttachment {
+	public init(_ from: Caked.Configuration.DirectorySharingAttachment) {
+		self.init()
+
+		self._source = from.source
+
+		if from.hasName {
+			self._name = from.name
+		}
+
+		if from.hasDestination {
+			self._destination = from.destination
+		}
+		
+		if from.hasUid {
+			self._uid = Int(from.uid)
+		}
+
+		if from.hasGid {
+			self._gid = Int(from.gid)
+		}
+	}
+}
+
+extension BridgeAttachement {
+	public init(_ from: Caked.Configuration.BridgeAttachment) {
+		self.init(network: from.network, mode: .init(argument: from.mode), macAddress: from.hasMacAddress ? from.macAddress : nil)
+	}
+}
+
+extension SocketDevice {
+	public init(_ from: Caked.Configuration.SocketDevice) {
+		self.init(mode: .init(from.mode)!, port: Int(from.port), bind: from.bind)
+	}
+}
+
+extension MappedPort.Proto {
+	public init(_ from: String) {
+		switch from {
+		case "tcp":
+			self = .tcp
+		case "udp":
+			self = .udp
+		case "both":
+			self = .both
+		default:
+			self = .none
+		}
+	}
+}
+
+extension TunnelAttachement {
+	public init(_ from: Caked.Configuration.TunnelAttachement) {
+		self.init()
+		switch from.port {
+		case .forward(let value):
+			self.init(host: Int(value.hostPort), guest: Int(value.guestPort), proto: .init(value.protocol))
+		case .unixDomain(let value):
+			self.init(host: value.hostPath, guest: value.guestPath, proto: .init(value.protocol))
+		default:
+			self.oneOf = .none
+		}
+	}
+}
+
+
+public struct CakedConfiguration: VirtualMachineConfiguration, Codable, Identifiable, Hashable {
+	public var id: URL {
+		self.locationURL
+	}
+
+	public var locationURL: URL
+	public var version: Int
+	public var os: VirtualizedOS
+	public var arch: Architecture
+	public var diskSize: Int
+	public var cpuCountMin: Int
+	public var suspendable: Bool
+	public var cpuCount: Int
+	public var memorySizeMin: UInt64
+	public var memorySize: UInt64
+	public var macAddress: String?
+	public var source: ImageSource
+	public var osName: String?
+	public var osRelease: String?
+	public var dynamicPortForwarding: Bool
+	public var displayRefit: Bool
+	public var instanceID: String
+	public var dhcpClientID: String?
+	public var sshPrivateKeyPath: String?
+	public var sshPrivateKeyPassphrase: String?
+	public var configuredUser: String
+	public var configuredPassword: String?
+	public var configuredGroup: String
+	public var configuredGroups: [String]?
+	public var configuredPlatform: SupportedPlatform
+	public var clearPassword: Bool
+	public var ifname: Bool
+	public var autostart: Bool
+	public var agent: Bool
+	public var firstLaunch: Bool
+	public var nested: Bool
+	public var attachedDisks: [DiskAttachement]
+	public var mounts: DirectorySharingAttachments
+	public var networks: [BridgeAttachement]
+	public var useCloudInit: Bool
+	public var sockets: [SocketDevice]
+	public var console: ConsoleAttachment?
+	public var forwardedPorts: [TunnelAttachement]
+	public var runningIP: String?
+	public var display: ViewSize
+	public var vncPassword: String
+
+	#if arch(arm64)
+	public var ecid: Data?
+	public var hardwareModel: Data?
+	#endif
+
+	public init(_ from: Caked.Configuration) {
+		// Map fields directly when available on `from`. For fields not present, use safe defaults.
+		self.locationURL = URL(fileURLWithPath: "/dev/null")
+		self.version = Int(from.version)
+		self.os = .init(from.os)!
+		self.arch = .init(from.arch)!
+		self.diskSize = Int(from.diskSize)
+		self.cpuCountMin = Int(from.cpuCountMin)
+		self.suspendable = from.suspendable
+		self.cpuCount = Int(from.cpuCount)
+		self.memorySizeMin = from.memorySizeMin
+		self.memorySize = from.memorySize
+		self.macAddress = from.macAddress
+		self.source = .init(from.source)!
+		self.osName = from.osName
+		self.osRelease = from.osRelease
+		self.dynamicPortForwarding = from.dynamicPortForwarding
+		self.displayRefit = from.displayRefit
+		self.instanceID = from.instanceID
+		self.dhcpClientID = from.dhcpClientID
+		self.sshPrivateKeyPath = from.sshPrivateKeyPath
+		self.sshPrivateKeyPassphrase = from.sshPrivateKeyPassphrase
+		self.configuredUser = from.configuredUser
+		self.configuredPassword = from.configuredPassword
+		self.configuredGroup = from.configuredGroup
+		self.configuredGroups = from.configuredGroups
+		self.configuredPlatform = .init(from.configuredPlatform)
+		self.clearPassword = from.clearPassword_p
+		self.ifname = from.ifname
+		self.autostart = from.autostart
+		self.agent = from.agent
+		self.firstLaunch = from.firstLaunch
+		self.nested = from.nested
+		self.attachedDisks = from.attachedDisks.map({.init($0)})
+		self.mounts = from.mounts.map({.init($0)})
+		self.networks = from.networks.map({.init($0)})
+		self.useCloudInit = from.useCloudInit
+		self.sockets = from.sockets.map({.init($0)})
+		if from.hasConsole {
+			self.console = .init(argument: from.console.url)
+		}
+		self.forwardedPorts = from.forwardedPorts.map({.init($0)})
+		self.runningIP = from.runningIp
+		if from.hasDisplay {
+			let display = from.display
+			
+			self.display = .init(width: Int(display.width), height: Int(display.height))
+		} else {
+			self.display = .init(width: 1024, height: 768)
+		}
+		self.vncPassword = from.vncPassword
+		
+		if from.hasEcid {
+			self.ecid = from.ecid
+		}
+		
+		if from.hasHardwareModel {
+			self.hardwareModel = from.hardwareModel
+		}
+	}
+}
+
+extension Caked.Configuration {
+	var configuration: CakedConfiguration {
+		CakedConfiguration(self)
+	}
+}
+
 
 extension Caked_VirtualMachineStatus: CustomStringConvertible {
 	public var description: String {
