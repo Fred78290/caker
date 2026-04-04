e5395ecc6a5309582cbc6b859476e8990fb07024
Fred78290 <frederic.boltz@gmail.com>
2026-04-04 15:12:24 +0200

    Adds script for wiki publishing and updates token usage
    Introduces a new script to automate running the wiki publishing workflow using act, ensuring consistent environment setup.
    Updates the existing workflow script to retrieve the GitHub token dynamically via the GitHub CLI, improving security and reducing manual steps.

--- Scripts/act-wiki.sh
+++ Scripts/act-wiki.sh
@@ -2,10 +2,10 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
-act push --workflows "${PROJECT_ROOT}/.github/workflows/release.yaml" \
+act push --workflows "${PROJECT_ROOT}/.github/workflows/publish-wiki.yaml" \
 	--secret GITHUB_TOKEN="$(gh auth token)" \
 	--secret-file "${PROJECT_ROOT}/.env"\
 	--var-file "${PROJECT_ROOT}/.vars" \
-	--platform self-hosted="-self-hosted" \
+	--platform ubuntu-latest=catthehacker/ubuntu:act-latest \
 	--eventpath "${PROJECT_ROOT}/act.json" \
 	--local-repository "https://github.com/Fred78290/caker@snapshot=${PROJECT_ROOT}"
