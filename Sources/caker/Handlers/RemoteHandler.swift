3ab95925ed25011462f5c33adacf93f7cf267a87
Fred78290 <frederic.boltz@gmail.com>
2026-04-24 15:43:50 +0200

    Add remote deletion support to RemoteHandler
    Extend the RemoteHandler to support deleting remotes via the gRPC service client. This allows users to manage remote configurations by sending a delete command through the client connection, consistent with the existing list functionality.

--- Sources/caker/Handlers/RemoteHandler.swift
+++ Sources/caker/Handlers/RemoteHandler.swift
@@ -4,11 +4,22 @@ import GRPCLib
 
 extension RemoteHandler {
 	public static func listRemote(client: CakedServiceClient?, runMode: Utils.RunMode) throws -> ListRemoteReply {
-
+		
 		guard let client = client else {
 			return self.listRemote(runMode: runMode)
 		}
-
+		
 		return try ListRemoteReply(client.remote(.with { $0.command = .list}).response.wait().remotes.list)
+	}
+	
+	public static func deleteRemote(client: CakedServiceClient?, name: String, runMode: Utils.RunMode) throws -> DeleteRemoteReply {
+		guard let client = client else {
+			return self.deleteRemote(name: name, runMode: runMode)
+		}
+		
+		return try DeleteRemoteReply(client.remote(.with {
+			$0.command = .delete
+			$0.deleteRequest = name
+		}).response.wait().remotes.deleted)
 	}
 }
