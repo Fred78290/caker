d544ad0f92624be9a059ef06bcf12baa96930fa9
Fred78290 <frederic.boltz@gmail.com>
2026-05-17 16:44:51 +0200

    Improve cache and remote object deletion
    Refactors the deletion logic to correctly distinguish between local VM paths and URLs referring to cached images or remote entries. This includes adding support for OCI image deletion and refining handling for other cache types and remote databases. The `tryDeleteLocal` function now consistently returns a `DeletedObject`.

--- Sources/cakedlib/Handlers/DeleteHandler.swift
+++ Sources/cakedlib/Handlers/DeleteHandler.swift
@@ -9,7 +9,7 @@ import GRPCLib
 import NIO
 
 public struct DeleteHandler {
-	static func tryDeleteLocal(name: String, runMode: Utils.RunMode) -> DeletedObject? {
+	static func tryDeleteLocal(name: String, runMode: Utils.RunMode) -> DeletedObject {
 		func doIt(_ location: VMLocation) throws -> DeletedObject {
 			if case .running = location.status {
 				return DeletedObject(source: "vm", name: location.name, deleted: false, reason: String(localized: "VM is running"))
@@ -25,7 +25,7 @@ public struct DeleteHandler {
 			} else if let vmURL = URL(string: name) {
 				let location: VMLocation
 				
-				if vmURL.isFileURL || (vmURL.scheme.map { VMLocation.supportedSchemes.contains($0) } ?? false) {
+				if vmURL.isFileURL || VMLocation.supportedSchemes.contains(vmURL.scheme) {
 					location = try VMLocation.newVMLocation(vmURL: vmURL, runMode: runMode)
 				} else {
 					return DeletedObject(source: "vm", name: name, deleted: false, reason: String(localized: "VM not found"))
@@ -37,48 +37,70 @@ public struct DeleteHandler {
 			return DeletedObject(source: "vm", name: name, deleted: false, reason: error.reason)
 		}
 
-		return nil
+		return DeletedObject(source: "unknown", name: name, deleted: false, reason: String(localized: "Unsupported URL"))
 	}
 
 	public static func delete(names: [String], runMode: Utils.RunMode) throws -> [DeletedObject] {
-		return try names.compactMap { name in
-			guard let result = tryDeleteLocal(name: name, runMode: runMode) else {
-				if let u = URL(string: name) {
-					var purgeableStorages: [String: CommonCacheImageCache] = [
-						CloudImageCache.scheme: try CloudImageCache(runMode: runMode),
-						RawImageCache.scheme: try RawImageCache(runMode: runMode),
-						IPSWCache.scheme: try IPSWCache(runMode: runMode),
-						IsoCache.scheme: try IsoCache(runMode: runMode),
-						SimpleStreamsImageCache.scheme: try SimpleStreamsImageCache(runMode: runMode),
-					]
-
-					if true {
-						let remoteDb = try Home(runMode: runMode).remoteDatabase()
-
-						try remoteDb.keys.forEach {
-							purgeableStorages[$0] = try SimpleStreamsImageCache(runMode: runMode)
-						}
-					}
-
-					if let scheme = u.scheme, let cache = purgeableStorages[scheme] {
-						let purgeables = try cache.purgeables()
-
-						if let purgeable = purgeables.first(where: { cache.fqn($0).contains(u.absoluteString) }) {
-							try purgeable.delete()
-							return DeletedObject(source: cache.location, name: purgeable.name, deleted: true, reason: String.empty)
-						}
-						return DeletedObject(source: cache.location, name: u.lastPathComponent, deleted: false, reason: String(localized: "Object not found"))
-					} else if let scheme = u.scheme {
-						return DeletedObject(source: scheme, name: name, deleted: false, reason: String(localized: "Unsupported URL scheme"))
-					} else {
-						return DeletedObject(source: "vm", name: name, deleted: false, reason: String(localized: "VM not found"))
-					}
+		func listRemotes() throws -> [String:SimpleStreamsImageCache] {
+			let remoteDb = try Home(runMode: runMode).remoteDatabase()
+			var imageCaches: [String: SimpleStreamsImageCache] = [:]
+			
+			try remoteDb.remote.forEach { (key, value) in
+				if let url = URL(string: value), let name = url.host(percentEncoded: false) {
+					imageCaches[key] = try SimpleStreamsImageCache(name: name, runMode: runMode)
 				}
-
-				return DeletedObject(source: "unknown", name: name, deleted: false, reason: String(localized: "Unsupported URL"))
 			}
 
-			return result
+			return imageCaches
+		}
+
+		return try names.compactMap { name in
+			if let u = URL(string: name), let scheme = u.scheme, VMLocation.supportedSchemes.contains(scheme) == false {
+				let remotes = try listRemotes()
+				let purgeableStorages: [String: CommonCacheImageCache] = [
+					CloudImageCache.scheme: try CloudImageCache(runMode: runMode),
+					RawImageCache.scheme: try RawImageCache(runMode: runMode),
+					IPSWCache.scheme: try IPSWCache(runMode: runMode),
+					IsoCache.scheme: try IsoCache(runMode: runMode),
+					OCIImageCache.scheme: try OCIImageCache(runMode: runMode),
+					SimpleStreamsImageCache.scheme: try SimpleStreamsImageCache(runMode: runMode),
+				]
+
+				if scheme == OCIImageCache.scheme {
+					let contentStore = try PurgeableContentStore(runMode: runMode)
+					let purgeables = try contentStore.purgeables()
+
+					if let purgeable = purgeables.first(where: {
+						contentStore.fqn($0).contains(u.absoluteString)
+					}) {
+						try purgeable.delete()
+
+						return DeletedObject(source: scheme, name: purgeable.name, deleted: true, reason: String.empty)
+					}
+				}
+				
+				if let cache = purgeableStorages[scheme] {
+					let purgeables = try cache.purgeables()
+					
+					if let purgeable = purgeables.first(where: { cache.fqn($0).contains(u.absoluteString) }) {
+						try purgeable.delete()
+						return DeletedObject(source: cache.location, name: purgeable.name, deleted: true, reason: String.empty)
+					}
+					
+				} else if let cache = remotes[scheme] {
+					if let entry = cache.findCache(fingerprintOrAlias: u.vmName) {
+						try cache.deleteCache(fingerprint: entry.fingerprint)
+
+						return DeletedObject(source: cache.location, name: name, deleted: true, reason: String.empty)
+					}
+				} else {
+					return DeletedObject(source: scheme, name: name, deleted: false, reason: String(localized: "Unsupported URL scheme"))
+				}
+
+				return DeletedObject(source: scheme, name: u.lastPathComponent, deleted: false, reason: String(localized: "Object not found"))
+			}
+
+			return tryDeleteLocal(name: name, runMode: runMode)
 		}
 	}
 
